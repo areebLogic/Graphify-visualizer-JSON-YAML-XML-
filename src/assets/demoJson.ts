e507574c03c7621685693827f18acab4020c8059
areebLogic <areebLogic@users.noreply.github.com>
2022-07-17 07:13:38 +0500

    Added via commit

---
+++ src/assets/demoJson.ts
@@ -0,0 +1,33 @@
+const json = `{
+  "id": "0987654321",
+  "isActive": "yes",
+  "age": 18,
+  "name": "My Bro",
+  "email": "mybro@gmail.com",
+  "countries": [
+    "Australia",
+    "Pakistan"
+  ],
+  "friends": [
+    {
+      "id": 0,
+      "name": "My Other Bro",
+      "email": "myotherbro@gmail.com",
+      "countries": [
+        "USA",
+        "Hungary"
+      ]
+    },
+    {
+      "id": 2,
+      "name": "My Sis",
+      "email": "mysis@gmail.com",
+      "countries": [
+        "Japan",
+        "Iran"
+      ]
+    }
+  ]
+}`
+
+export default json;
